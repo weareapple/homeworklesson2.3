c4668b5a1ad51fa139a25cc3b25015005d7f39a1
weareapple <jabub@mail.ru>
2020-09-10 23:49:14 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+    int age = 30;
+    int temperature = 20;
+    boolean isShinee = false;
+
+
+            System.out.println("Можно идти гулять");
+
+
+
+
+    public static String home(int age, int temperature){
+
+
+        }
+    }
+}
