1dd4a2fc36aac9c94ab87ecc26a8f6bac6b86ef1
weareapple <jabub@mail.ru>
2020-09-14 13:59:25 +0600

    homework2lessonRuslan

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,38 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(homeWork(geterateRandomAge(), 22));
+        System.out.println(homeWork(geterateRandomAge(), 20));
+        System.out.println(homeWork(geterateRandomAge(), -22));
+        System.out.println(homeWork(geterateRandomAge(), 99));
+        System.out.println(homeWork(geterateRandomAge(), 6));
+
     }
 
-    int age = 30;
-    int temperature = 20;
-    boolean isShinee = false;
+    public static String homeWork(int age, int temperature) {
+        System.out.println("Возраст: " + age + " Температура: " + temperature);
 
 
-            System.out.println("Можно идти гулять");
-
-
-
-
-    public static String home(int age, int temperature){
-
-
+        if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
+            return  "Можно идти гулять";
+        } else if (age > 20 && temperature >= 0 && temperature <= 28) {
+            return  "Можно идти гулять";
+        } else if (age > 45 && temperature >= -10 && temperature <= 25) {
+            return  "Можно идти гулять";
+        } else {
+            return "Оставайтесь дома";
         }
+
     }
+    public static int geterateRandomAge(){
+        Random random = new Random();
+        return random.nextInt(70);
+    }
+
 }
+
+
